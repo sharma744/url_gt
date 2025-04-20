093235fbc5f98510a0f63cd5e6df2f2176ee4d8e
sharma744 <aayusharma9711@gmail.com>
2025-04-20 17:59:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,30 +1,33 @@
 let mongoose=require("mongoose")
 let express=require("express")
 let db2=require("./model/model2.js")
+let fn=async()=>{
+    for(let i=0;i<203;i++){
+       let room={
+        roomno:i+1
+       }
+       await db2.create(room);;
+
+}
+}
+let fun=async()=>{
 // const url=mongodb+srv://nodeuser:<db_password>@cluster0.ag3fwai.mongodb.net/
-mongoose.connect("mongodb+srv://nodeuser:Yahoo123@cluster0.ag3fwai.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
+ await mongoose.connect("mongodb+srv://nodeuser:Yahoo123@cluster0.ag3fwai.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
 let db=mongoose.connection;
-let app=express();
-app.use(express.json())
+await fn()
 db.once('open',()=>{
 console.log("database connected");
 })
 db.on('error',()=>{
     console.log("data base coneection error");
 })
-// let fn=async()=>{
-//     for(let i=0;i<203;i++){
-//        let room={
-//         roomno:i+1
-//        }
-//         db2.insertOne(room);
-
-// }
-// }
-// fn();
+let app=express();
+app.use(express.json())
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 require("./routes/route1.js")(app);
 app.listen(3000,()=>{
     console.log("server connected succesfully");
 })
+}
+fun();
