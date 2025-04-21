99df93a6bab0d4522eaf526911d4f3ebf0c272b8
sharma744 <aayusharma9711@gmail.com>
2025-04-21 23:56:30 +0530

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,6 @@ let fn=async()=>{
 }
 }
 let fun=async()=>{
-// const url=mongodb+srv://nodeuser:<db_password>@cluster0.ag3fwai.mongodb.net/
   mongoose.connect("mongodb+srv://nodeuser:Yahoo123@cluster0.ag3fwai.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
 let db=mongoose.connection;
 fn()
